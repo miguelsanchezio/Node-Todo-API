900ab886a7fc9156c1a003749cd9c89a7977e63d
Miguel Sanchez <msanchez.anaheim@gmail.com>
2017-12-21 13:28:50 -0800

    Added GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -7,14 +8,6 @@ const { User } = require('./models/user');
 
 const app = express();
 app.use(bodyParser.json());
-
-app.get('/todos', (req, res) => {
-    Todo.find().then(todos => {
-        res.send({todos})
-    }, e => {
-        res.status(400).send(e);
-    });
-});
 
 app.post('/todos', (req, res) => {
     const todo = new Todo({
@@ -26,6 +19,29 @@ app.post('/todos', (req, res) => {
     }, e => {
         res.status(400).send(e);
     })
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then(todos => {
+        res.send({todos})
+    }, e => {
+        res.status(400).send(e);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send({});
+    }
+
+    Todo.findById(id).then(todo => {
+        if(!todo) {
+            res.status(404).send({});
+        }
+        return res.send({todo})
+    }).catch(e => res.status(400).send({}));
 });
 
 if(!module.parent) {
