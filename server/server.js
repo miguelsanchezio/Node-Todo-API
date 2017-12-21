2390ef22ffe819eba2d41b781a4ca1fc98fe1b40
Miguel Sanchez <msanchez.anaheim@gmail.com>
2017-12-21 15:13:54 -0800

    Added DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -35,15 +35,30 @@ app.get('/todos/:id', (req, res) => {
     const id = req.params.id;
 
     if(!ObjectID.isValid(id)) {
-        return res.status(404).send({});
+        return res.status(404).send();
     }
 
     Todo.findById(id).then(todo => {
         if(!todo) {
-            return res.status(404).send({});
+            return res.status(404).send();
         }
-        return res.send({todo})
-    }).catch(e => res.status(400).send({}));
+        res.send({todo})
+    }).catch(e => res.status(400).send());
+});
+
+app.delete('/todos/:id', (req, res) => {
+    const id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then(todo => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch(e => res.status(400).send());
 });
 
 if(!module.parent) {
