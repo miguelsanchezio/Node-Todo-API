c16b580918302cf1e98a1789ceba2abf7ffcba66
Miguel Sanchez <msanchez.anaheim@gmail.com>
2017-12-22 13:08:44 -0800

    Added GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -33,16 +33,16 @@ var UserSchema = new mongoose.Schema({
 }, { usePushEach: true });
 
 UserSchema.methods.toJSON = function () {
-    var user = this;
-    var userObject = user.toObject();
+    const user = this;
+    const userObject = user.toObject();
 
     return _.pick(userObject, ['_id', 'email']);
 }
 
 UserSchema.methods.generateAuthToken = function () {
-    var user = this;
-    var access = 'auth';
-    var token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
+    const user = this;
+    const access = 'auth';
+    const token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
 
     user.tokens.push({access, token});
 
@@ -50,6 +50,23 @@ UserSchema.methods.generateAuthToken = function () {
         return token;
     });
 };
+
+UserSchema.statics.findByToken = function (token) {
+    const User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (e) {
+        return Promise.reject('test');
+    }
+
+    return User.findOne({
+        _id: decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+}
 
 const User = mongoose.model('User', UserSchema)
 
